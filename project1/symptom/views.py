438a0714cc4f6ae51b798cfd5a8077df5916c447
Abhishek <vyasa887@gmail.com>
2020-04-15 02:34:49 +0530

    api integration update

--- project1/symptom/views.py
+++ project1/symptom/views.py
@@ -5,10 +5,30 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import auth,User
 from symptom.models import contact
+
+# Encryption libraries required for
+# getting token before each api request
+import base64
+import hashlib,hmac
+
+# api request library
+import requests
+
+
+'''
+This is the most important file , please check and maintain this before anything
+else. Please try to mention my name in contributors section in your project ,
+...just kidding.
+btw my name's abhishek.
+'''
+
+
 def index(request):
     return render(request,'index.html')
+
 def covid(request):
     return render(request,'covid19.html')
+
 def contact1(request):
     if request.method=='POST':
         first_name=request.POST.get('firstname','')
@@ -21,9 +41,6 @@ def contact1(request):
         return render(request,'contact.html')
     else:
         return render(request,'contact.html')
-
-
-
 
 def login(request):
     if request.method == 'POST':
@@ -60,7 +77,7 @@ def registration(request):
                 return redirect('registration.html')
             else:
                 user=User.objects.create_user(username=username,email=email,password=password1,first_name=first_name,last_name=last_name)
-                user.save();
+                user.save()
                 print("user created")
                 return redirect('login.html')
         else:
@@ -68,3 +85,53 @@ def registration(request):
             return redirect('registration.html')
     else:
         return render(request,'registration.html')
+
+
+#Call this function each time before making api request and pass it in the request url
+# E.x : https://sandbox-healthservice.priaid.ch/symptoms?token=  <--this is where you need your token.
+
+def create_token():
+    # your apimedic credentials's username
+    username="My_Username_here"
+    # your apimedic credentials's password 
+    password="My_password_here"
+
+    #Don't change this !
+    req_uri='https://sandbox-authservice.priaid.ch/login'
+
+    #some encryption for making a post request , to ask for token from apimedic
+    raw_hash=hmac.new(password.encode("utf-8"),req_uri.encode("utf-8"),hashlib.md5)
+    computed_hash=base64.b64encode(raw_hash.digest())
+
+    header={
+        'Authorization': f"Bearer {username}:{computed_hash.decode('utf-8')}",
+    }
+
+    myreq=requests.post(req_uri,headers=header)
+    response=myreq.json()
+
+    #check your cmd window , this is your api token 
+    #you need to create this each time before making
+    #request to apimedic
+    print(response['Token'])
+
+    return response['Token']
+
+
+'''
+Parameter 	    Type 	Values
+
+symptoms 	    JSON    encoded int[] array 	Serialized array of selected symptom ids in json format. example symptoms=[234,235,236]
+
+gender 	        string 	male, female
+
+year_of_birth 	int 	
+
+'''
+def my_diagnosis(symptoms,gender,year_of_birth):
+    token=create_token()
+    # This is how you make a api call/request
+    response=requests.get('https://sandbox-healthservice.priaid.ch/diagnosis',token=token,language='en-gb',symptoms=symptoms,gender=gender,year_of_birth=year_of_birth)
+    # if status_code == 200 then be happy else google the http status_codes
+    print(response.status_code)
+    return response.json()
